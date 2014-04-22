a233c4e2272495d8a2894ae0939aadb8dc391cc9
animosity <bogdan.radacina@gmail.com>
2014-04-22 20:51:52 +1000

    LKM now loads when a usb keyboard gets plugged in.

--- ExampleLKM/helloWorld.c
+++ ExampleLKM/helloWorld.c
@@ -1,20 +1,37 @@
-#include <linux/init.h>
+
 #include <linux/module.h>
+#include <linux/usb.h>
 
-MODULE_LICENSE("Dual BSD/GPL");
+#define DRIVER_AUTHOR "Bogdan Radacina"
+#define DRIVER_DESC "Demo Driver for USB Keyboard interface class"
 
-static int mod_init(void)
+#define USB_INTERFACE_CLASS_AND_PROTOCOL(cl, pr) \
+	.match_flags = USB_DEVICE_ID_MATCH_INT_CLASS | USB_DEVICE_ID_MATCH_INT_PROTOCOL , \
+	.bInterfaceClass = (cl), \
+	.bInterfaceProtocol = (pr)
+
+
+static __init int hello_init(void) 
 {
-	pr_debug("HelloWorld!\n");
+	printk(KERN_DEBUG "HelloWorld loaded.");
 	return 0;
 }
 
-static void mod_exit(void)
+static __exit void hello_exit(void)
 {
-	pr_debug("HelloWorld unloaded\n");
+	printk(KERN_DEBUG "HelloWorld exiting.");
 }
 
-module_init(mod_init);
-module_exit(mod_exit);
+static const struct usb_device_id usbkbd_ids[] = {
+	{ USB_INTERFACE_CLASS_AND_PROTOCOL(0x03, 0x01) }, /* USB Keyboard class */
+	{ }						/* Terminating entry */
+};
 
-MODULE_ALIAS("usb:v*p*d*dc*dsc*dp*ic03isc*ip01in*");
+MODULE_DEVICE_TABLE(usb, usbkbd_ids);
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_AUTHOR(DRIVER_AUTHOR);
+MODULE_DESCRIPTION(DRIVER_DESC);
+MODULE_LICENSE("GPL");
