a25c0ae7c3fd61d24382e412546bcfe4a846d196
animosity <bogdan.radacina@gmail.com>
2014-05-03 14:37:10 +1000

    Fixed format of char device driver

--- CharDeviceDriver/charDeviceDriver.c
+++ CharDeviceDriver/charDeviceDriver.c
@@ -3,11 +3,11 @@
 #include <linux/miscdevice.h>
 #include <linux/fs.h>
 #include <uapi/linux/errno.h>
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
 #include <linux/string.h>
 
 #define DRIVER_AUTHOR "Bogdan Radacina"
-#define DRIVER_DESC "Demo Driver for a Char device driver" 
+#define DRIVER_DESC "Demo Driver for a Char device driver"
 #define DRIVER_NAME "eudyptula"
 #define MY_ID "25e6eec82542"
 #define MY_ID_CHAR_COUNT 12
@@ -27,20 +27,20 @@ static ssize_t do_read(struct file *file, char __user *buf, size_t count,
 			   loff_t *ppos)
 {
 	ssize_t toRead = 0;
-	
+
 	if ((*ppos) >= MY_ID_CHAR_COUNT) {
 		goto out;
 	}
 
-	if (count < MY_ID_CHAR_COUNT ) {
+	if (count < MY_ID_CHAR_COUNT) {
 		toRead = count;
 	}
-	
+
 	if (count >= MY_ID_CHAR_COUNT) {
 		toRead = MY_ID_CHAR_COUNT;
 	}
-	
-	if(copy_to_user(buf, MY_ID, toRead)) {
+
+	if (copy_to_user(buf, MY_ID, toRead)) {
 		toRead = -EFAULT;
 		goto out;
 	}
@@ -58,15 +58,15 @@ static ssize_t do_write(struct file *file, const char __user *buf,
 		numWritten = -EINVAL;
 		goto out;
 	}
-	if(copy_from_user(myBuf, buf, MY_ID_CHAR_COUNT)) {
+	if (copy_from_user(myBuf, buf, MY_ID_CHAR_COUNT)) {
 		numWritten = -EFAULT;
 		goto out;
 	}
-	if(strncmp(myBuf,MY_ID, MY_ID_CHAR_COUNT)) {
+	if (strncmp(myBuf, MY_ID, MY_ID_CHAR_COUNT)) {
 		numWritten = -EINVAL;
 		goto out;
 	}
-	numWritten = count;	
+	numWritten = count;
 out:
 	return numWritten;
 }
@@ -74,17 +74,18 @@ out:
 static __init int hello_init(void)
 {
 	int error;
-	myDevice.minor = MISC_DYNAMIC_MINOR; 
+	myDevice.minor = MISC_DYNAMIC_MINOR;
 	myDevice.name = DRIVER_NAME;
-	myDevice.fops = &myDevice_fops; 
+	myDevice.fops = &myDevice_fops;
 
 	error = misc_register(&myDevice);
-	if(error) {
+	if (error) {
 		pr_err("eudyptula_dev: failed to register misc device\n");
 		return error;
 	}
 
-	pr_info("eudyptula_dev: Hello World Registered with Minor: %i\n", myDevice.minor);
+	pr_info("eudyptula_dev: Hello World Registered with Minor: %i\n",
+		myDevice.minor);
 	return 0;
 }
 
